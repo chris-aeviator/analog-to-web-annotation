806369b22f8862884474125abcc84822234150f0
Giulio Andreini <andreini@netseven.it>
2016-05-21 15:33:13 +0200

    Frontend implementations setup

---
+++ Gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+gulp.task('styles', function() {
+gulp.src('sass/style.scss')
+.pipe(sass().on('error', sass.logError))
+.pipe(gulp.dest('./'))
+});
+
+gulp.task('connect', function() {
+connect.server({
+root: './',
+livereload: true
+});
+});
+
+gulp.task('html', function () {
+gulp.src('./index.html')
+.pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+gulp.watch(['./index.html'], ['html']);
+});
+
+
+gulp.task('default',function() {
+gulp.watch('sass/*.scss',['styles']);
+});
+
+gulp.task('dev', ['styles', 'connect', 'watch']);
